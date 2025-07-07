4fc27f88cca4777fbf6657c05cb821b32abaed34
Umut Ozan Ozyildirim <uozanozyildirim@email.com>
2025-07-07 11:38:33 +0300

    #admin panel

--- admin-panel/services/api.js
+++ admin-panel/services/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://netcore-boilerplate:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    'Authorization': 'ApiKey ABC-xyz',
   },
 });
 
