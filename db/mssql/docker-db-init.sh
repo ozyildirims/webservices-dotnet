32ee73e53906f8bbc626347b17aa5bbf02363984
Umut Ozan Ozyildirim <uozanozyildirim@email.com>
2025-07-07 10:24:37 +0300

    #mssql ssl issue solved.

--- db/mssql/docker-db-init.sh
+++ db/mssql/docker-db-init.sh
@@ -4,15 +4,8 @@ set -e
 # Wait for SQL Server to be ready
 sleep 30s
 
-# Run the setup script
-/opt/mssql-tools18/bin/sqlcmd -b -S localhost -U sa -P SomeStrongPwd123 -Q "
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'limitkurs')
-BEGIN
-    CREATE DATABASE limitkurs;
-END
-GO
-USE limitkurs;
-GO"
+# Run the setup script with SSL disabled using connection string
+/opt/mssql-tools18/bin/sqlcmd -b -S "localhost" -U sa -P SomeStrongPwd123 -d master -Q "SET NOCOUNT ON; IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'limitkurs') BEGIN CREATE DATABASE limitkurs; END" -C
 
 # Make the script executable
 chmod +x /db-init.sh
